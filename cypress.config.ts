717e91959903e61cad5000a0e5d3ac5dd5cb757c
bennymeade <brendan_meade@yahoo.co.uk>
2023-05-12 23:18:43 +0200

    initial commit

---
+++ cypress.config.ts
@@ -0,0 +1,26 @@
+import { defineConfig } from 'cypress';
+import cypressSplit from 'cypress-split';
+
+export default defineConfig({
+  watchForFileChanges: false,
+  chromeWebSecurity: true,
+  numTestsKeptInMemory: 20,
+  viewportWidth: 1920,
+  viewportHeight: 1200,
+  video: false,
+  retries: {
+    runMode: 1,
+    openMode: 0,
+  },
+  pageLoadTimeout: 30000,
+  requestTimeout: 20000,
+  responseTimeout: 20000,
+  defaultCommandTimeout: 10000,
+
+  e2e: {
+    setupNodeEvents(on, config) {
+      cypressSplit(on, config)
+      return config
+    },
+  },
+});
