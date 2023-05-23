f95e251136bddca5ab4ec3c6ec5a4978d101bb49
bennymeade <brendan_meade@yahoo.co.uk>
2023-05-23 23:17:18 +0200

    add cypress split to mobile config

--- cypress/configs/cypress.mobile.config.ts
+++ cypress/configs/cypress.mobile.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from "cypress";
-import cypressSplit from 'cypress-split';
+// import cypressSplit from 'cypress-split';
 import defu from "defu";
 
 import defaultConfig from "../../cypress.config";
@@ -10,15 +10,14 @@ export default {
       {
         viewportWidth: 375,
         viewportHeight: 812,
-        userAgent:
-          "Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) Mobile/15E148",
+        userAgent: "Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) Mobile/15E148",
         
-        e2e: {
-          setupNodeEvents(on, config) {
-            cypressSplit(on, config)
-            return config
-          },
-        },
+        // e2e: {
+        //   setupNodeEvents(on, config) {
+        //     cypressSplit(on, config)
+        //     return config
+        //   },
+        // },
       },
       defaultConfig
     )
